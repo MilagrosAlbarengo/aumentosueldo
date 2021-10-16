7ab7f9f9ae1093fe89c8d0dee5c042cf90b4073d
MilagrosAlbarengo <92064009+MilagrosAlbarengo@users.noreply.github.com>
2021-10-16 20:29:06 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+let sueldo: number = document.getElementById("precio");
+let aumento1: number = document.getElementById("aumento1");
+let aumento2: number = document.getElementById("aumento2");
+let aumento3: number = document.getElementById("aumento3");
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  if (sueldo.value >= 0 a 15000) {
+
+    switch <variable>{
+      case <número1>: 
+     <instrucciones>;
+     break;
+      
+             case <número2>: 
+     <instrucciones>;
+     break; 
+                 <...> 
+                 default: <instrucciones>
+     }
+    console.log(precio.value - (precio.value * 0.1));
+  } else {
+    console.log(precio.value);
+  }
+});
